3a65ef388d9e0e81cec27c2478745a882d92c234
Nishad <nishad@kodelint.com>
2025-05-30 10:47:50 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:sample_project/quiz.dart';
+
+
+
+void main(){
+  runApp(
+   Quiz()
+  );
+}
---
+++ lib/questions_screeen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class QuestionsScreeen extends StatefulWidget {
+  const QuestionsScreeen({super.key});
+
+  @override
+  State<QuestionsScreeen> createState() => _QuestionsScreeenState();
+}
+
+class _QuestionsScreeenState extends State<QuestionsScreeen> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Questions Screen');
+  }
+}
